e836a11358aa56b2ed5845220719fbf06b0dc07c
anantharjun672001-a11y <anantharjun672001@gmail.com>
2025-11-24 19:30:41 +0530

    Getting Elements From HTML

---
+++ script.js
@@ -0,0 +1,18 @@
+// Getting Elements From HTML
+const dice = document.getElementById("dice");
+const playerMove = document.getElementById("player-move");
+const player1Score = document.getElementById("player1-score");
+const player2Score = document.getElementById("player2-score");
+const player1Button = document.getElementById("player1-button");
+const player2Button = document.getElementById("player2-button");
+//Start Function
+
+//Update Moves Function
+
+//Dice Roll Logic
+
+//Update Score Logic
+
+//Winning Condition
+
+//Resetting the game
